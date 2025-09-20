c5ee90fea55072cdc01ec3a380f914474b621523
Chikanma Chika-Onuegbu <chikanmaonuegbu@ymail.com>
2025-09-20 10:04:07 -0600

    interface

--- cdk-project.go
+++ cdk-project.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -18,12 +19,22 @@ func NewCdkProjectStack(scope constructs.Construct, id string, props *CdkProject
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	awslambda.NewFunction(stack, jsii.String("myLambdaFunction"), &awslambda.FunctionProps{
+	table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName:   jsii.String("userTable"),
+	})
+
+	myFunction := awslambda.NewFunction(stack, jsii.String("myLambdaFunction"), &awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
 		Handler: jsii.String("main"),
 		Code:    awslambda.Code_FromAsset(jsii.String("lambda/function.zip"), nil),
 	})
 
+	table.GrantReadWriteData(myFunction)
+	
 	// example resource
 	// queue := awssqs.NewQueue(stack, jsii.String("CdkProjectQueue"), &awssqs.QueueProps{
 	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
